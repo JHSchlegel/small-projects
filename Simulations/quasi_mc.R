69012974672d9c99cfc4d0edb0f2227885787702
Jan Schlegel <janschlegel@bluewin.ch>
2023-08-24 23:48:03 +0200

    added section about mc integration

--- Simulations/quasi_mc.R
+++ Simulations/quasi_mc.R
@@ -1,3 +1,9 @@
+# This file demonstrates important differences between quasi and pseudo random
+# numbers
+
+# Packages and Presets ----
+
+
 library(qrng)
 library(tidyverse)
 library(cowplot)
@@ -5,6 +11,8 @@ library(ggthemes)
 
 theme_set(ggthemes::theme_economist())
 
+
+# Plotting Quasi and Pseudo Random Numbers ----
 set.seed(123)
 rng.df <- data.frame(x.pr = runif(1000), y.pr = runif(1000))
 rng.df[, c("x.qr", "y.qr")] <- qrng::ghalton(1000, d = 2)
@@ -27,6 +35,56 @@ p_combined
 
 ggsave("./Simulations/Plots/qrng_vs_prng.pdf", plot = p_combined, width = 10,
        height = 4.5)
+
+
+
+# Monte Carlo Integration ---- 
+# Let's calculate expectation of exp(abs(x - 2))
+# the interval [-1, 3]
+
+n.sim <- 1000000
+
+set.seed(123)
+X.qr <- -1 + 4 * qrng::ghalton(n.sim)
+
+set.seed(123)
+X.pr <- runif(n.sim, -1, 3)
+
+
+Y <- function(x) 4 * exp(-abs(x-2))
+
+mcint.df <- data.frame(X.qr = X.qr, Y.qr = Y(X.qr), X.pr = X.pr, Y.pr = Y(X.pr))
+
+
+mcint_long <- mcint.df %>% 
+  rowid_to_column("n") %>% 
+  pivot_longer(-n, names_to = "type", values_to = "value")
+
+mcint_long %>% 
+  group_by(type) %>% 
+  summarize(
+    mean = mean(value),
+    sd = sd(value),
+    p25 = quantile(value, 0.25),
+    median = median(value),
+    p75 = quantile(value, 0.75)
+  )
+
+mcint.df %>% 
+  ggplot(aes(x = X.qr, y = Y.qr)) + 
+  geom_line()
+
+# Importance Sampling ----
+
+
+
+
+
+
+
+
+
+
 
 
 
