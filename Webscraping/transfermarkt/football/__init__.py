6e67f5cb1fdace8c0728da4fa957f4b822f6dc1e
Jan Schlegel <jschlegel@student.ethz.ch>
2024-03-30 15:47:01 +0100

    deleted unecessary files

--- Webscraping/transfermarkt/football/__init__.py
+++
@@ -1,6 +0,0 @@
-import datetime
-import logging
-print(f"Initializing Webdriver at local time: {datetime.datetime.now().strftime('%H:%M:%S')}")
-
-# set up logging
-logging.basicConfig(filename = "transfermarkt.log", level = logging.INFO)
--- Webscraping/transfermarkt/football/constants.py
+++
@@ -1,4 +0,0 @@
-# This file contains constants used in the project
-
-BASE_URL = "https://www.transfermarkt.ch/spieler-statistik/wertvollstemannschaften/marktwertetop"
-N_PAGES = 4
